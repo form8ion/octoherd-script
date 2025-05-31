aa7a652669311ae1f44c43a2feff635f8160ece3
Matt Travi <programmer@travi.org>
2025-05-30 22:12:05 -0500

    refactor: define a name for the scaffold function

--- src/scaffolder.js
+++ src/scaffolder.js
@@ -1,6 +1,6 @@
 import {promises as fs} from 'fs';
 
-export default async function ({projectRoot}) {
+export default async function scaffold({projectRoot}) {
   await fs.writeFile(`${projectRoot}/index.js`, 'export async function script(octokit, repository) {}');
 
   return {tags: ['octoherd-script']};
